ef86ad9de0d7d92b767d9480f3dfdb2ecbaa899c
QuangVD2 <quangvd2@vng.com.vn>
2025-04-01 17:04:58 +0700

    refactor: remove unused classes

--- src/module/board/models/index.ts
+++
@@ -1,6 +0,0 @@
-export { CardModel } from './CardModel';
-export { DeckModel } from './DeckModel';
-export { HandModel } from './HandModel';
-export { DiscardPileModel } from './DiscardPileModel';
-export { BoardModel } from './BoardModel';
-export * from './types';
--- src/module/board/views/index.ts
+++
@@ -1,4 +0,0 @@
-export { DeckView } from './DeckView';
-export { HandView } from './HandView';
-export { CardView } from './CardView';
-export { DiscardPileView } from './DiscardPileView';
